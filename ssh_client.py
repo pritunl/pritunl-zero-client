5cb5d7dfa8fad7a11070f572a0b1a6f877393fc8
Zachary Huff <zach.huff.386@gmail.com>
2017-12-27 01:19:04 -0500

    Create new release

--- ssh_client.py
+++ ssh_client.py
@@ -8,7 +8,7 @@ import urlparse
 import sys
 import datetime
 
-VERSION = '1.0.763.39'
+VERSION = '1.0.764.26'
 SSH_DIR = '~/.ssh'
 CONF_PATH = SSH_DIR + '/pritunl-zero.json'
 DEF_KNOWN_HOSTS_PATH = '~/.ssh/known_hosts'
