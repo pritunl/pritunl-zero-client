54754b61d1dfa197211a70da2ac3b506b247800f
Zachary Huff <zach.huff.386@gmail.com>
2018-06-18 01:21:17 -0400

    Create new release

--- ssh_client.py
+++ ssh_client.py
@@ -8,7 +8,7 @@ import urlparse
 import sys
 import datetime
 
-VERSION = '1.0.860.27'
+VERSION = '1.0.937.22'
 SSH_DIR = '~/.ssh'
 CONF_PATH = SSH_DIR + '/pritunl-zero.json'
 BASH_PROFILE_PATH = '~/.bash_profile'
