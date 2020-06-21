23a8db8186d6a416c5f5f6a997e3e22738f23b85
Zachary Huff <zach.huff.386@gmail.com>
2020-06-20 21:00:26 -0400

    Create new release

--- ssh_host_client.py
+++ ssh_host_client.py
@@ -18,7 +18,7 @@ try:
 except:
     HAS_BOTO = False
 
-VERSION = '1.0.1623.92'
+VERSION = '1.0.1671.4'
 CONF_PATH = '/etc/pritunl-ssh-host.json'
 DEF_SSH_CONF_PATH = '/etc/ssh/sshd_config'
 DEF_PUB_KEY_CONF_PATH = '/etc/ssh/ssh_host_rsa_key.pub'
